edb8f75fadf6422646de6a37585d5dbae7e8c845
erick-cip <erickcip001@gmail.com>
2024-08-16 14:28:47 -0300

    Questões Resolvidas

---
+++ 3.js
@@ -0,0 +1,8 @@
+const livro ={
+    titulo: "João e Maria",
+    autor:"irmãos Grimm",
+    ano:1812
+}
+
+console.log(`Livro:${livro.titulo} \nAutor:${livro.autor}`)
+console.log("teste")
---
+++ 4.js
@@ -0,0 +1,7 @@
+const produto = {
+    nome: "Coca-Cola 2L",
+    preço: 10,
+    quantidade: 10
+}
+
+console.log(`Produto:${produto.nome}\nValor do estoque:${produto.preço*produto.quantidade} R$`)
